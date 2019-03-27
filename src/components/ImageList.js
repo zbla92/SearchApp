1f4eb3c5dbb4fa3af10d92062f4ec9b22dee2c55
zbla92 <milanblaz2580456@gmail.com>
2019-03-27 08:47:25 -0700

    finilazed v1

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular} alt="alst" />
+    const images = props.images.map( ({ description, id, urls }) => {
+        
+        return <img key={id} src={urls.regular} alt={description} />
     })
 
     return <div>{images}</div>;
