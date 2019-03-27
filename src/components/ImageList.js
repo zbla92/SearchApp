a53eb81ba1491c816a813eb6b6548800a8bb8915
Blaz <mb250653@ncr.com>
2019-03-27 18:04:18 -0400

    First Commit V2

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
+import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-    const images = props.images.map( ({ description, id, urls }) => {
+    const images = props.images.map( image => {
         
-        return <img key={id} src={urls.regular} alt={description} />
+        return <ImageCard key={image.id} image={image} />
     })
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
